a67bf1a95bf212a057ab9f69b301989d0cb19a69
Edward <edwardconw@gmail.com>
2019-09-15 14:08:04 -0500

    Creacion de Clases

---
+++ src/main/java/com/mycompany/carreraderelevos/Equipo.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.carreraderelevos;
+
+/**
+ *
+ * @author SuperUs
+ */
+public class Equipo {
+    
+    private String nombreEquipo;
+    private String posicion1;
+    private String posicion2;
+    private String posicion3;
+
+    public Equipo() {
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getPosicion1() {
+        return posicion1;
+    }
+
+    public void setPosicion1(String posicion1) {
+        this.posicion1 = posicion1;
+    }
+
+    public String getPosicion2() {
+        return posicion2;
+    }
+
+    public void setPosicion2(String posicion2) {
+        this.posicion2 = posicion2;
+    }
+
+    public String getPosicion3() {
+        return posicion3;
+    }
+
+    public void setPosicion3(String posicion3) {
+        this.posicion3 = posicion3;
+    }
+    
+    
+    
+}
